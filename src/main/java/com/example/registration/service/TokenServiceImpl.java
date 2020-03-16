bbde97565d8cde06fb8114474233335152aed6d5
AlexTkachukPC <Alexander.k.Tkachuk@gmail.com>
2020-03-16 15:53:44 +0200

    security jwt first commit

--- src/main/java/com/example/registration/service/TokenServiceImpl.java
+++ src/main/java/com/example/registration/service/TokenServiceImpl.java
@@ -2,6 +2,8 @@ package com.example.registration.service;
 
 import com.example.registration.domain.Role;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.impl.TextCodec;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,6 +17,10 @@ public class TokenServiceImpl implements TokenService {
                 .setSubject("Registration")
                 .claim("name", username)
                 .claim("roles", roles)
+                .signWith(
+                        SignatureAlgorithm.HS256,
+                        TextCodec.BASE64.decode("RegistrationS")
+                )
                 .compact();
     }
 }
